47428902aa652de4acc5844e9a2aa0141d842e07
Abe <abhi.rawat456@gmail.com>
2021-12-26 17:55:51 +0530

    feat: added components

---
+++ src/components/Input.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let placeholder = ''
+  export let id: string
+  export let value: string
+  export let onInput: (e: Event) => void
+</script>
+
+<textarea
+  type="text"
+  name={id}
+  {id}
+  class="w-full p-4 border border-gray-300 rounded-lg focus:ring-indigo-800 focus:border-indigo-800"
+  rows="3"
+  {placeholder}
+  bind:value
+  on:input={onInput}
+/>
