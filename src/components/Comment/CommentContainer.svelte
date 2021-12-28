21553c352a1f906bcfc9cf69cd623b3381048172
Abe <abhi.rawat456@gmail.com>
2021-12-28 22:42:05 +0530

    feat: added Reply component

---
+++ src/components/Comment/CommentContainer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { Comment as TComment, Reply as TReply } from '@types'
+  import Comment from './Comment.svelte'
+  import Reply from './Reply.svelte'
+
+  export let comment: TComment
+  export let replies: Array<TReply>
+</script>
+
+<div class="space-y-4">
+  <Comment data={comment} />
+  <div class="space-y-4 pl-4 sm:pl-8 sm:ml-8 border-l-2 border-l-gray-200">
+    {#each replies as reply}
+      <Reply data={reply} />
+    {/each}
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -20,7 +20,7 @@
 </script>
 
 <script lang="ts">
-  import Comment from '@components/Comment/Comment.svelte'
+  import CommentContainer from '@components/Comment/CommentContainer.svelte'
   import CommentInput from '@components/Comment/CommentInput.svelte'
   import type { UserData } from '@types'
 
@@ -35,7 +35,7 @@
 <div class="bg-gray-100 min-h-screen">
   <div class="max-w-screen-md mx-auto px-4 py-8 space-y-4">
     {#each comments as comment}
-      <Comment data={comment} />
+      <CommentContainer {comment} replies={comment.replies} />
     {/each}
     <CommentInput avatar={currentUser.image.png} btnText="Send" />
   </div>
